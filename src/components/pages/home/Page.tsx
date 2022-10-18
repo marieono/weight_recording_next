5aa8324e787d2682129dcb24e9cd6b4a4547b53c
Yuto Ono <stellarwindmaster@gmail.com>
2022-10-18 20:46:12 +0900

    ESLintのエラーを修正

--- src/components/pages/home/Page.tsx
+++ src/components/pages/home/Page.tsx
@@ -1,13 +1,8 @@
 import type { NextPage } from "next"
-import { css } from "@emotion/react"
 import Header from "../../common/Header"
 
 const Page: NextPage = () => {
   return <Header />
 }
-
-const helloStyle = css({
-  color: "blue",
-})
 
 export default Page
