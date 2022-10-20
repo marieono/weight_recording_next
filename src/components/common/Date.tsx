48dc383cd120fd8856d615ad4b66466d5f6def78
marieono <104805318+marieono@users.noreply.github.com>
2022-10-20 22:38:15 +0900

    SVGアイコン追加

--- src/components/common/Date.tsx
+++ src/components/common/Date.tsx
@@ -15,7 +15,33 @@ const Date = () => {
         background-color: #e6e6e6;
       `}
     >
+      <svg
+        width="32"
+        height="32"
+        viewBox="0 0 32 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M19.705 11.41L18.295 10L12.295 16L18.295 22L19.705 20.59L15.125 16L19.705 11.41Z"
+          fill="black"
+          fill-opacity="0.54"
+        />
+      </svg>
       2022 9.19 MON
+      <svg
+        width="32"
+        height="32"
+        viewBox="0 0 32 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M13.705 10L12.295 11.41L16.875 16L12.295 20.59L13.705 22L19.705 16L13.705 10Z"
+          fill="black"
+          fill-opacity="0.54"
+        />
+      </svg>
     </div>
   )
 }
