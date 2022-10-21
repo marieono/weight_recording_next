c8facf2518f83890eb67975a8ebf409b5424e833
marieono <104805318+marieono@users.noreply.github.com>
2022-10-21 09:03:15 +0900

    fill-opacity="0.54"をコメントアウト

--- src/components/common/Date.tsx
+++ src/components/common/Date.tsx
@@ -25,7 +25,7 @@ const Date = () => {
         <path
           d="M19.705 11.41L18.295 10L12.295 16L18.295 22L19.705 20.59L15.125 16L19.705 11.41Z"
           fill="black"
-          fill-opacity="0.54"
+          // fill-opacity="0.54"
         />
       </svg>
       2022 9.19 MON
@@ -39,7 +39,7 @@ const Date = () => {
         <path
           d="M13.705 10L12.295 11.41L16.875 16L12.295 20.59L13.705 22L19.705 16L13.705 10Z"
           fill="black"
-          fill-opacity="0.54"
+          // fill-opacity="0.54"
         />
       </svg>
     </div>
