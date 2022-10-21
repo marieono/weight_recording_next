d324db17044983a4a47af4d4af19bbdee1e92e33
marieono <104805318+marieono@users.noreply.github.com>
2022-10-21 11:52:17 +0900

    登録ボタン追加

---
+++ src/components/common/Forwordbutton.tsx
@@ -0,0 +1,19 @@
+const Forwordbutton = () => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M13.705 10L12.295 11.41L16.875 16L12.295 20.59L13.705 22L19.705 16L13.705 10Z"
+        fill="black"
+        // fill-opacity="0.54"
+      />
+    </svg>
+  )
+}
+
+export default Forwordbutton
