0a0e7c9f5f75cbfe0589ef5f382b793c90ba4b54
marieono <104805318+marieono@users.noreply.github.com>
2022-10-21 09:03:15 +0900

    fill-opacity="0.54"をコメントアウト

--- src/components/common/Weight_input.tsx
+++ src/components/common/Weight_input.tsx
@@ -31,24 +31,3 @@ const Weight_input = () => {
 }
 
 export default Weight_input
-
-// import { css } from "@emotion/react"
-
-// const Header = () => {
-//   return (
-//     <div
-//       css={css`
-//         display: flex;
-//         justify-content: center;
-//         align-items: center;
-//         font-weight: bold;
-//         height: 50px;
-//         width: 100vw;
-//         background-color: #e6e6e6;
-//       `}
-//     >
-//       入力
-//     </div>
-//   )
-// }
-// export default Header
