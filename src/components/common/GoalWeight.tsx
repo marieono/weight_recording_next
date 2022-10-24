13880e1b9020ae42085c45f21917d9710110422c
marieono <104805318+marieono@users.noreply.github.com>
2022-10-24 15:47:50 +0900

    身長、目標体重コンポーネント追加

---
+++ src/components/common/GoalWeight.tsx
@@ -0,0 +1,36 @@
+import { css } from "@emotion/react"
+import Forwordbutton from "./Forwordbutton"
+
+const GoalWeight = () => {
+  return (
+    <>
+      <div
+        css={css`
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+          font-size: 18px;
+          font-weight: bold;
+          height: 90px;
+          width: 335px auto;
+        `}
+      >
+        目標体重
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+            font-size: 18px;
+            font-weight: bold;
+            height: 90px;
+            width: 335px auto;
+          `}
+        >
+          kg <Forwordbutton />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default GoalWeight
---
+++ src/components/common/Height.tsx
@@ -0,0 +1,36 @@
+import { css } from "@emotion/react"
+import Forwordbutton from "./Forwordbutton"
+
+const Height = () => {
+  return (
+    <>
+      <div
+        css={css`
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+          font-size: 18px;
+          font-weight: bold;
+          height: 90px;
+          width: 335px auto;
+        `}
+      >
+        身長
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+            font-size: 18px;
+            font-weight: bold;
+            height: 90px;
+            width: 335px auto;
+          `}
+        >
+          cm <Forwordbutton />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Height
