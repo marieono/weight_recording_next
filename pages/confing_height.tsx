712a9f11cb7a7437d2d2ad6a10c1a56c66a8ed44
marieono <104805318+marieono@users.noreply.github.com>
2022-10-28 15:24:32 +0900

    身長入力ページ作ろうとしたが途中で終了

---
+++ pages/confing_height.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next"
+import Page from "../src/components/pages/cofingheight"
+
+const ConfigHeight: NextPage = () => {
+  return <Page />
+}
+
+export default ConfigHeight
---
+++ src/components/pages/cofingheight/Page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next"
+import Header from "../../common/Header"
+
+const Page: NextPage()=>{
+return(
+  <>
+  <Header title="設定"/>
+  </>
+  )
+}
+export default Page
