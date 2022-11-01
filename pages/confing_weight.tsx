b8835e52df862b6db2c4bc0bb8a1cb917459a188
marieono <104805318+marieono@users.noreply.github.com>
2022-11-01 11:06:10 +0900

    身長入力ページ、体重入力ページ追加

---
+++ pages/confing_weight.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next"
+import Page from "../src/components/pages/confingweight"
+
+const ConfingWeight: NextPage = () => {
+  return <Page />
+}
+
+export default ConfingWeight
--- src/components/pages/confingweight/Page.tsx
+++ src/components/pages/confingweight/Page.tsx
@@ -1,15 +1,16 @@
 import { NextPage } from "next"
 import FooterSet from "../../common/FooterSet"
+import GoalWeight_input from "../../common/GoalWeight_input"
 import Header from "../../common/Header"
-import Height_input from "../../common/Height_input"
 
 const Page: NextPage = () => {
   return (
     <>
       <Header title="設定" />
-      <Height_input />
+      <GoalWeight_input />
       <FooterSet />
     </>
   )
 }
+
 export default Page
