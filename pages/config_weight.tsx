0bb0b328069e34b062eb723ede41e3646aefc849
marieono <104805318+marieono@users.noreply.github.com>
2022-11-01 11:42:37 +0900

    configスペルミス修正

---
+++ pages/config_weight.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next"
+import Page from "../src/components/pages/configweight"
+
+const ConfigWeight: NextPage = () => {
+  return <Page />
+}
+
+export default ConfigWeight
