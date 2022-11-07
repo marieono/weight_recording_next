50846103558f7a0520e2f552a2b58c3205dc2fd0
marieono <104805318+marieono@users.noreply.github.com>
2022-11-07 10:40:43 +0900

    体重入力欄、目標体重、身長input実装

--- src/components/common/Weight_input.tsx
+++ src/components/common/Weight_input.tsx
@@ -16,15 +16,18 @@ const Weight_input = () => {
       `}
     >
       体重
-      <div
-        css={css`
-          margin: 15px;
-          height: 40px;
-          width: 83px;
-          background-color: white;
-          border: solid #d9d9d9 1px;
-        `}
-      ></div>
+      <div>
+        <input
+          type="text"
+          css={css`
+            margin: 15px;
+            height: 40px;
+            width: 83px;
+            background-color: white;
+            border: solid #d9d9d9 1px;
+          `}
+        ></input>
+      </div>
       kg
     </div>
   )
