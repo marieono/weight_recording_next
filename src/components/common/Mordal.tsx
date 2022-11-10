4f04f0ccd3e22afd12795ce1e7bec6203be0a834
marieono <104805318+marieono@users.noreply.github.com>
2022-11-10 14:26:16 +0900

    モーダルCSS作成

--- src/components/common/Mordal.tsx
+++ src/components/common/Mordal.tsx
@@ -4,10 +4,45 @@ const Mordal = () => {
   return (
     <div
       css={css`
-        width: 260px auto;
+        width: 260px;
         height: 120px;
+        margin: 65px auto;
+        border: #e6e6e6;
+        border-radius: 10px;
+        box-shadow: 2px 2px 4px #e6e6e6;
+        background-color: white;
+        text-align: center;
       `}
-    ></div>
+    >
+      <div
+        css={css`
+          /* display: table-cell;
+          vertical-align: middle; */
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding-top: 15px;
+        `}
+      >
+        入力完了
+      </div>
+      <div
+        css={css`
+          border-bottom: solid 1px #e6e6e6;
+          padding: 10px 0;
+        `}
+      ></div>
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding: 15px;
+        `}
+      >
+        閉じる
+      </div>
+    </div>
   )
 }
 
--- src/components/pages/home/Page.tsx
+++ src/components/pages/home/Page.tsx
@@ -4,6 +4,7 @@ import Header from "../../common/Header"
 import Date from "../../common/Date"
 import Weight_input from "../../common/Weight_input"
 import Registerbutton from "../../common/Registerbutton"
+import Mordal from "../../common/Mordal"
 
 const Page: NextPage = () => {
   return (
@@ -12,6 +13,7 @@ const Page: NextPage = () => {
       <Date />
       <Weight_input />
       <Registerbutton />
+      <Mordal />
       <Footer activeMenu="input" />
     </>
   )
