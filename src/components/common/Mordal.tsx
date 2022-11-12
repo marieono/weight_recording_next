0ea113dc905f0064468909d752cac026d100c9f2
marieono <104805318+marieono@users.noreply.github.com>
2022-11-13 08:23:05 +0900

    Modal１つ完成

--- src/components/common/Mordal.tsx
+++ src/components/common/Mordal.tsx
@@ -4,25 +4,39 @@ const Mordal = () => {
   return (
     <div
       css={css`
-        width: 260px;
-        margin: 65px auto;
-        border: #e6e6e6;
-        border-radius: 10px;
-        box-shadow: 2px 2px 4px #e6e6e6;
-        background-color: white;
-        text-align: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        z-index: 100;
+        width: 100vw;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        position: fixed;
+        background-color: rgba(0, 0, 0, 0.75);
       `}
     >
-      <div css={style}>入力完了</div>
       <div
-        css={
-          // (css`
-          //   border-top: solid 1px #e6e6e6;
-          // `)
-          style
-        }
+        css={css`
+          width: 260px;
+          margin: 65px auto;
+          border: #e6e6e6;
+          border-radius: 10px;
+          box-shadow: 2px 2px 4px #e6e6e6;
+          background-color: white;
+          text-align: center;
+        `}
       >
-        閉じる
+        <div css={style}>入力完了</div>
+        <div
+          css={css`
+            ${style};
+            border-top: solid 1px #e6e6e6;
+          `}
+        >
+          閉じる
+        </div>
       </div>
     </div>
   )
--- src/components/pages/registermodal/Page.tsx
+++ src/components/pages/registermodal/Page.tsx
@@ -5,7 +5,6 @@ import Date from "../../common/Date"
 import Weight_input from "../../common/Weight_input"
 import Registerbutton from "../../common/Registerbutton"
 import Mordal from "../../common/Mordal"
-import { css } from "@emotion/react"
 
 const Page: NextPage = () => {
   return (
@@ -16,16 +15,6 @@ const Page: NextPage = () => {
       <Registerbutton />
       <Mordal />
       <Footer activeMenu="input" />
-      <div
-        css={css`
-          width: 100vw;
-          height: 100vh;
-          top: 0;
-          left: 0;
-          position: fixed;
-          background-color: rgba(0, 0, 0, 0.75);
-        `}
-      ></div>
     </>
   )
 }
