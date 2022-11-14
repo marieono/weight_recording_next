7de8d582bf72a5f75f091cc176de4b28cba932d6
marieono <104805318+marieono@users.noreply.github.com>
2022-11-14 11:42:26 +0900

    登録ボタンクリックでモーダル画面表示

--- src/components/common/Registerbutton.tsx
+++ src/components/common/Registerbutton.tsx
@@ -1,24 +1,27 @@
 import { css } from "@emotion/react"
+import Link from "next/link"
 
 const Registerbutton = () => {
   return (
-    <div
-      css={css`
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin: 50px auto;
-        font-weight: bold;
-        height: 50px;
-        width: 250px;
-        border-radius: 30px;
-        background-color: #f95791;
-        opacity: 0.5;
-        color: white;
-      `}
-    >
-      登録する
-    </div>
+    <Link href="/registermordal" passHref>
+      <a
+        css={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          margin: 50px auto;
+          font-weight: bold;
+          height: 50px;
+          width: 250px;
+          border-radius: 30px;
+          background-color: #f95791;
+          opacity: 0.5;
+          color: white;
+        `}
+      >
+        登録する
+      </a>
+    </Link>
   )
 }
 export default Registerbutton
