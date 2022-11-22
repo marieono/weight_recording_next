88c2a3e615902e632d0cefe77eda6433f9faa7d0
marieono <104805318+marieono@users.noreply.github.com>
2022-11-22 10:11:25 +0900

    表コンポーネントerror

--- src/components/common/Table.tsx
+++ src/components/common/Table.tsx
@@ -2,60 +2,150 @@ import { css } from "@emotion/react"
 
 const Table = () => {
   return (
-    <table
+    <div
       css={css`
-        border-collapse: collapse;
+        display: flex;
+        justify-content: center;
+        align-items: center;
       `}
     >
-      <tr>
-        <th
-          css={css`
-            ${th}
-          `}
-        >
-          日付
-        </th>
-        <th
-          css={css`
-            ${th}
-          `}
-        >
-          体重
-        </th>
-      </tr>
-      <tr>
-        <td
-          css={css`
-            ${td}
-          `}
-        >
-          1
-        </td>
-        <td
-          css={css`
-            ${td}
-          `}
-        >
-          50.0
-        </td>
-      </tr>
-    </table>
+      <table
+        css={css`
+          ${tb}
+        `}
+      >
+        <tr>
+          <th
+            css={css`
+              ${th};
+              width: 70px;
+            `}
+          >
+            日付
+          </th>
+          <th
+            css={css`
+              ${th};
+              width: 90px;
+            `}
+          >
+            体重
+          </th>
+        </tr>
+        <tr>
+          <td
+            css={css`
+              ${tdpink}
+            `}
+          >
+            1
+          </td>
+          <td
+            css={css`
+              ${tdpink}
+            `}
+          >
+            50.0
+          </td>
+        </tr>
+        <tr>
+          <td
+            css={css`
+              ${tdwhite}
+            `}
+          >
+            2
+          </td>
+          <td
+            css={css`
+              ${tdwhite}
+            `}
+          >
+            50.0
+          </td>
+        </tr>
+      </table>
+      <table
+        css={css`
+          ${tb}
+        `}
+      >
+        <tr>
+          <th
+            css={css`
+              ${th};
+              width: 70px;
+            `}
+          >
+            日付
+          </th>
+          <th
+            css={css`
+              ${th};
+              width: 90px;
+            `}
+          >
+            体重
+          </th>
+        </tr>
+        <tr>
+          <td
+            css={css`
+              ${tdpink}
+            `}
+          >
+            16
+          </td>
+          <td
+            css={css`
+              ${tdpink}
+            `}
+          >
+            50.0
+          </td>
+        </tr>
+        <tr>
+          <td
+            css={css`
+              ${tdwhite}
+            `}
+          >
+            17
+          </td>
+          <td
+            css={css`
+              ${tdwhite}
+            `}
+          >
+            50.0
+          </td>
+        </tr>
+      </table>
+    </div>
   )
 }
 
 export default Table
 
-// const tb = css`
-//   border-bottom: solid 2px #d9d9d9;
-// `
+const tb = css`
+  border-collapse: collapse;
+  margin: 25px 10px;
+`
 
 const th = css`
   color: white;
   background: #f95791;
-  border-bottom: solid 2px #d9d9d9;
+  border-bottom: solid 1px #d9d9d9;
+  text-align: center;
 `
 
-const td = css`
+const tdpink = css`
   background: #fee6ef;
   border-bottom: solid 1px #d9d9d9;
+  text-align: center;
+`
+const tdwhite = css`
+  background: white;
+  border-bottom: solid 1px #d9d9d9;
+  text-align: center;
 `
