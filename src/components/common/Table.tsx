553f307148742c87ae24ad9aa0afdc5d6ee2f542
marieono <104805318+marieono@users.noreply.github.com>
2022-11-22 10:51:44 +0900

    表コンポーネントerror修正

--- src/components/common/Table.tsx
+++ src/components/common/Table.tsx
@@ -14,112 +14,120 @@ const Table = () => {
           ${tb}
         `}
       >
-        <tr>
-          <th
-            css={css`
-              ${th};
-              width: 70px;
-            `}
-          >
-            日付
-          </th>
-          <th
-            css={css`
-              ${th};
-              width: 90px;
-            `}
-          >
-            体重
-          </th>
-        </tr>
-        <tr>
-          <td
-            css={css`
-              ${tdpink}
-            `}
-          >
-            1
-          </td>
-          <td
-            css={css`
-              ${tdpink}
-            `}
-          >
-            50.0
-          </td>
-        </tr>
-        <tr>
-          <td
-            css={css`
-              ${tdwhite}
-            `}
-          >
-            2
-          </td>
-          <td
-            css={css`
-              ${tdwhite}
-            `}
-          >
-            50.0
-          </td>
-        </tr>
+        <thead>
+          <tr>
+            <th
+              css={css`
+                ${th};
+                width: 70px;
+              `}
+            >
+              日付
+            </th>
+            <th
+              css={css`
+                ${th};
+                width: 90px;
+              `}
+            >
+              体重
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td
+              css={css`
+                ${tdpink}
+              `}
+            >
+              1
+            </td>
+            <td
+              css={css`
+                ${tdpink}
+              `}
+            >
+              50.0
+            </td>
+          </tr>
+          <tr>
+            <td
+              css={css`
+                ${tdwhite}
+              `}
+            >
+              2
+            </td>
+            <td
+              css={css`
+                ${tdwhite}
+              `}
+            >
+              50.0
+            </td>
+          </tr>
+        </tbody>
       </table>
       <table
         css={css`
           ${tb}
         `}
       >
-        <tr>
-          <th
-            css={css`
-              ${th};
-              width: 70px;
-            `}
-          >
-            日付
-          </th>
-          <th
-            css={css`
-              ${th};
-              width: 90px;
-            `}
-          >
-            体重
-          </th>
-        </tr>
-        <tr>
-          <td
-            css={css`
-              ${tdpink}
-            `}
-          >
-            16
-          </td>
-          <td
-            css={css`
-              ${tdpink}
-            `}
-          >
-            50.0
-          </td>
-        </tr>
-        <tr>
-          <td
-            css={css`
-              ${tdwhite}
-            `}
-          >
-            17
-          </td>
-          <td
-            css={css`
-              ${tdwhite}
-            `}
-          >
-            50.0
-          </td>
-        </tr>
+        <thead>
+          <tr>
+            <th
+              css={css`
+                ${th};
+                width: 70px;
+              `}
+            >
+              日付
+            </th>
+            <th
+              css={css`
+                ${th};
+                width: 90px;
+              `}
+            >
+              体重
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td
+              css={css`
+                ${tdpink}
+              `}
+            >
+              16
+            </td>
+            <td
+              css={css`
+                ${tdpink}
+              `}
+            >
+              50.0
+            </td>
+          </tr>
+          <tr>
+            <td
+              css={css`
+                ${tdwhite}
+              `}
+            >
+              17
+            </td>
+            <td
+              css={css`
+                ${tdwhite}
+              `}
+            >
+              50.0
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
@@ -137,6 +145,7 @@ const th = css`
   background: #f95791;
   border-bottom: solid 1px #d9d9d9;
   text-align: center;
+  line-height: 30px;
 `
 
 const tdpink = css`
