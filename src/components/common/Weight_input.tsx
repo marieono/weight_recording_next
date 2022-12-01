bca508f88e1bf828af259709e55fae24ba919293
marieono <104805318+marieono@users.noreply.github.com>
2022-12-01 15:45:39 +0900

    react-hook-form実装途中

--- src/components/common/Weight_input.tsx
+++ src/components/common/Weight_input.tsx
@@ -1,6 +1,16 @@
 import { css } from "@emotion/react"
+import { useForm } from "react-hook-form"
+import { WeightRecord } from "./types"
 
 const Weight_input = () => {
+  const {
+    register,
+    handleSubmit,
+    resetField,
+    setValue,
+    formState: { errors, isValid },
+  } = useForm<WeightRecord>({ mode: "onChange" })
+
   return (
     <div
       css={css`
@@ -17,17 +27,26 @@ const Weight_input = () => {
     >
       体重
       <div>
-        <input
-          type="tel"
-          pattern="\d.?"
-          css={css`
-            margin: 15px;
-            height: 40px;
-            width: 83px;
-            background-color: white;
-            border: solid #d9d9d9 1px;
-          `}
-        ></input>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="tel"
+            css={css`
+              margin: 15px;
+              height: 40px;
+              width: 83px;
+              background-color: white;
+              border: solid #d9d9d9 1px;
+            `}
+            {...register("weight", {
+              required: "入力してください。",
+              valueAsNumber: true,
+              validate: (value) =>
+                !isNaN(value) || "半角数字で入力してください。",
+            })}
+            error={errors.weight != null}
+            helperText={errors.weight?.message ?? " "}
+          ></input>
+        </form>
       </div>
       kg
     </div>
---
+++ src/components/common/types.ts
@@ -0,0 +1,4 @@
+export type WeightRecord = {
+  date: string
+  weight: number
+}
