007de4f02e47830053b6a54d18d3a5f891950889
marieono <104805318+marieono@users.noreply.github.com>
2023-03-05 09:07:40 +0900

    日付が当日表示させれない

--- src/components/common/Date.tsx
+++ src/components/common/Date.tsx
@@ -6,47 +6,52 @@ type Props = {
   title: string
 }
 
-const Date = ({ title }: Props) => {
-  return (
+const Date = ({ title }: Props) => (
+  <div
+    css={css`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-weight: bold;
+    `}
+  >
+    <div
+      css={css`
+        cursor: pointer;
+      `}
+    >
+      <Backbutton />
+    </div>
     <div
       css={css`
         display: flex;
         justify-content: center;
         align-items: center;
-        font-weight: bold;
+        margin: 22px 7px;
+        height: 43px;
+        width: 249px;
+        font-size: 17px;
+        background-color: #e6e6e6;
       `}
     >
-      <div
-        css={css`
-          cursor: pointer;
-        `}
-      >
-        <Backbutton />
-      </div>
-      <div
-        css={css`
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          margin: 22px 7px;
-          height: 43px;
-          width: 249px;
-          font-size: 17px;
-          background-color: #e6e6e6;
-        `}
-      >
-        {title}
-        <input type="date" />
-      </div>
-      <div
-        css={css`
-          cursor: pointer;
-        `}
-      >
-        <Forwordbutton />
-      </div>
+      {title}
+      <input type="date" {...getDateText} />
     </div>
-  )
-}
+    <div
+      css={css`
+        cursor: pointer;
+      `}
+    >
+      <Forwordbutton />
+    </div>
+  </div>
+)
 
 export default Date
+function getDateText(
+  arg0: any
+): JSX.IntrinsicAttributes &
+  import("react").ClassAttributes<HTMLInputElement> &
+  import("react").InputHTMLAttributes<HTMLInputElement> {
+  throw new Error("Function not implemented.")
+}
