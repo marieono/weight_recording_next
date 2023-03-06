8ca7d47f4a70ec9ddff037d626e2d1a1b04ba9c3
marieono <104805318+marieono@users.noreply.github.com>
2023-03-06 11:59:47 +0900

    日付を設定したいがうまくいかない。。

--- src/components/common/Date.tsx
+++ src/components/common/Date.tsx
@@ -1,57 +1,69 @@
 import { css } from "@emotion/react"
+import { useForm } from "react-hook-form"
 import Backbutton from "./Backbutton"
 import Forwordbutton from "./Forwordbutton"
+import { WeightRecord } from "./types"
 
 type Props = {
   title: string
 }
 
-const Date = ({ title }: Props) => (
-  <div
-    css={css`
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      font-weight: bold;
-    `}
-  >
-    <div
-      css={css`
-        cursor: pointer;
-      `}
-    >
-      <Backbutton />
-    </div>
-    <div
-      css={css`
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin: 22px 7px;
-        height: 43px;
-        width: 249px;
-        font-size: 17px;
-        background-color: #e6e6e6;
-      `}
-    >
-      {title}
-      <input type="date" {...getDateText} />
-    </div>
-    <div
-      css={css`
-        cursor: pointer;
-      `}
-    >
-      <Forwordbutton />
-    </div>
-  </div>
-)
+const Date = ({ title }: Props) => {
+  const {
+    setValue,
+    // formState:{errors,isValid}
+  } = useForm<WeightRecord>({ mode: "onChange" })
+
+  const onSubmit: SubmitHandler<WeightRecord> = async (data) => {
+    try {
+      await setValue("date", getDateText(new Date()))
+    } catch (e) {
+      alert(`FireStoreへの書き込み中にエラーが発生しました:${e}`)
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-weight: bold;
+        `}
+      >
+        <div
+          css={css`
+            cursor: pointer;
+          `}
+        >
+          <Backbutton />
+        </div>
+        <div
+          css={css`
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 22px 7px;
+            height: 43px;
+            width: 249px;
+            font-size: 17px;
+            background-color: #e6e6e6;
+          `}
+        >
+          {title}
+          <input type="date" defaultValue={getDateText(new Date())} />
+        </div>
+        <div
+          css={css`
+            cursor: pointer;
+          `}
+        >
+          <Forwordbutton />
+        </div>
+      </div>
+    </form>
+  )
+}
 
 export default Date
-function getDateText(
-  arg0: any
-): JSX.IntrinsicAttributes &
-  import("react").ClassAttributes<HTMLInputElement> &
-  import("react").InputHTMLAttributes<HTMLInputElement> {
-  throw new Error("Function not implemented.")
-}
