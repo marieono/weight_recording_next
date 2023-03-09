0ed3bd861f73a0412d741722ca602bcc5db7b8cc
marieono <104805318+marieono@users.noreply.github.com>
2023-03-09 15:49:02 +0900

    修正途中

--- src/components/common/Date.tsx
+++ src/components/common/Date.tsx
@@ -1,5 +1,5 @@
 import { css } from "@emotion/react"
-import { useForm } from "react-hook-form"
+import { useFormContext } from "react-hook-form"
 import Backbutton from "./Backbutton"
 import Forwordbutton from "./Forwordbutton"
 import { WeightRecord } from "./types"
@@ -7,62 +7,53 @@ import { WeightRecord } from "./types"
 type Props = {
   title: string
 }
-
 const Date = ({ title }: Props) => {
   const {
-    setValue,
+    register,
+    // setValue,
     // formState:{errors,isValid}
-  } = useForm<WeightRecord>({ mode: "onChange" })
-
-  const onSubmit: SubmitHandler<WeightRecord> = async (data) => {
-    try {
-      await setValue("date", getDateText(new Date()))
-    } catch (e) {
-      alert(`FireStoreへの書き込み中にエラーが発生しました:${e}`)
-    }
-  }
+  } = useFormContext<WeightRecord>()
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <div
+      css={css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-weight: bold;
+      `}
+    >
+      <div
+        css={css`
+          cursor: pointer;
+        `}
+      >
+        <Backbutton />
+      </div>
       <div
         css={css`
           display: flex;
           justify-content: center;
           align-items: center;
-          font-weight: bold;
+          margin: 22px 7px;
+          height: 43px;
+          width: 249px;
+          font-size: 17px;
+          background-color: #e6e6e6;
         `}
       >
-        <div
-          css={css`
-            cursor: pointer;
-          `}
-        >
-          <Backbutton />
-        </div>
-        <div
-          css={css`
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            margin: 22px 7px;
-            height: 43px;
-            width: 249px;
-            font-size: 17px;
-            background-color: #e6e6e6;
-          `}
-        >
+        <input type="date" {...register("date")}>
           {title}
-          <input type="date" defaultValue={getDateText(new Date())} />
-        </div>
-        <div
-          css={css`
-            cursor: pointer;
-          `}
-        >
-          <Forwordbutton />
-        </div>
+        </input>
       </div>
-    </form>
+      <div
+        css={css`
+          cursor: pointer;
+        `}
+      >
+        <Forwordbutton />
+      </div>
+    </div>
   )
 }
 
