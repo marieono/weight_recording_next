faab6209e040135b8826d57afbbb99d97fd215a9
marieono <104805318+marieono@users.noreply.github.com>
2023-03-10 10:03:18 +0900

    日付と体重のデータベース登録の紐づけ完了

--- src/components/pages/calendar/Page.tsx
+++ src/components/pages/calendar/Page.tsx
@@ -1,16 +1,14 @@
 import type { NextPage } from "next"
 import Footer from "../../common/Footer"
 import Header from "../../common/Header"
-import DateInput from "../../common/DateInput"
 import Table from "../../common/Table"
 
 const Page: NextPage = () => {
   return (
     <>
       <Header title="カレンダー" />
-      <Footer activeMenu="calendar" />
-      <DateInput />
       <Table />
+      <Footer activeMenu="calendar" />
     </>
   )
 }
