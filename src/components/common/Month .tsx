d9ea9310cd40d02a066fe043c6c1257bf8ef9322
marieono <104805318+marieono@users.noreply.github.com>
2023-03-13 15:10:23 +0900

    日付と年のコンポーネント作成

--- src/components/common/Month .tsx
+++ src/components/common/Month .tsx
@@ -1,10 +1,52 @@
+import { css } from "@emotion/react"
+import Backbutton from "./Backbutton"
+import Forwordbutton from "./Forwordbutton"
+
 const Month = () => {
-  const today = new Date()
-  const currentMonth = today.getMonth() + 1
+  const now = new Date()
+  const year = now.getFullYear()
+  const currentMonth = now.getMonth() + 1
 
   return (
     <>
-      <div>{currentMonth}</div>
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-weight: bold;
+        `}
+      >
+        <div
+          css={css`
+            cursor: pointer;
+          `}
+        >
+          <Backbutton />
+        </div>
+
+        <div
+          css={css`
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 22px 7px;
+            height: 43px;
+            width: 249px;
+            font-size: 17px;
+            background-color: #e6e6e6;
+          `}
+        >
+          {year}年{currentMonth}月
+        </div>
+        <div
+          css={css`
+            cursor: pointer;
+          `}
+        >
+          <Forwordbutton />
+        </div>
+      </div>
     </>
   )
 }
--- src/components/pages/calendar/Page.tsx
+++ src/components/pages/calendar/Page.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from "next"
 import Footer from "../../common/Footer"
 import Header from "../../common/Header"
+import Month from "../../common/Month "
 import Table from "../../common/Table"
 
 const Page: NextPage = () => {
   return (
     <>
       <Header title="カレンダー" />
+      <Month />
       <Table />
       <Footer activeMenu="calendar" />
     </>
